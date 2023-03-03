bc7fc9476251ec178f66b8abb79e25f17e44b467
Alvaro Romeral <alrompin98@gmail.com>
2023-03-04 00:45:23 +0100

    Actualizando Rock

--- class/Rock.hpp
+++ class/Rock.hpp
@@ -1,251 +1,244 @@
-class FortunateSon {
-	name = "Creedence Clearwater - FortunateSon";
-	sound[] = {"RomeMusic\RockClassic\FortunateSon.ogg",db+0,1};
-	duration=141;
-	musicClass = "RockClassic";
-};
-
-class RockYouLikeaHurricane {
-	name = "Scorpions - Rock You Like a Hurricane";
-	sound[] = {"RomeMusic\RockClassic\RockYouLikeaHurricane.ogg",db+0,1};
-	duration=254;
-	musicClass = "RockClassic";
-};
-
-class Celebration {
-	name = "Kool & The Gang - Celebration";
-	sound[] = {"RomeMusic\RockClassic\Celebration.ogg",db+0,1};
-	duration=258;
-	musicClass = "RockClassic";
-};
-
-class BadMoonRising {
-	name = "Creedence Clearwater - Bad Moon Rising";
-	sound[] = {"RomeMusic\RockClassic\BadMoonRising.ogg",db+0,1};
-	duration=141;
-	musicClass = "RockClassic";
-};
-
-class RunThroughTheJungle {
-	name = "Creedence Clearwater - Run Through The Jungle";
-	sound[] = {"RomeMusic\RockClassic\RunThroughTheJungle.ogg",db+0,1};
-	duration=186;
-	musicClass = "RockClassic";
-};
-
-class September {
-	name = "Earth, Wind & Fire - September";
-	sound[] = {"RomeMusic\RockClassic\September.ogg",db+0,1};
-	duration=215;
-	musicClass = "RockClassic";
-};
-
-class TheClashShouldIStayorShouldIGo {
-	name = "The Clash - Should I Stay or Should I Go";
-	sound[] = {"RomeMusic\RockClassic\TheClash-ShouldIStayorShouldIGo.ogg",db+0,1};
-	duration=189;
-	musicClass = "RockClassic";
-};
-
-class StatusQuoInTheArmyNow {
-	name = "Status Quo - In The Army Now";
-	sound[] = {"RomeMusic\RockClassic\StatusQuo-InTheArmyNow.ogg",db+0,1};
-	duration=257;
-	musicClass = "RockClassic";
-};
-
-class StatusQuoWhateveryouwant {
-	name = "Status Quo - Whatever you want";
-	sound[] = {"RomeMusic\RockClassic\StatusQuoWhateveryouwant.ogg",db+0,1};
-	duration=261;
-	musicClass = "RockClassic";
-};
-
-class ColdChiselKheSanh {
-	name = "Cold Chisel - KheSanh";
-	sound[] = {"RomeMusic\RockClassic\ColdChisel-KheSanh.ogg",db+0,1};
-	duration=250;
-	musicClass = "RockClassic";
-};
-
-class RedgumIWasOnly19 {
-	name = "Redgum - I Was Only 19";
-	sound[] = {"RomeMusic\RockClassic\Redgum-IWasOnly19.ogg",db+0,1};
-	duration=266;
-	musicClass = "RockClassic";
-};
-
-class StandingOnTheOutside {
-	name = "Standing On The Outside";
-	sound[] = {"RomeMusic\RockClassic\StandingOnTheOutside.ogg",db+0,1};
-	duration=176;
-	musicClass = "RockClassic";
-};
-
-class TheJimiHendrixExperienceAllAlongTheWatchtower {
-	name = "The Jimi Hendrix Experience - All Along The Watchtower";
-	sound[] = {"RomeMusic\RockClassic\TheJimiHendrixExperience-AllAlongTheWatchtower.ogg",db+0,1};
-	duration=241;
-	musicClass = "RockClassic";
-};
-
-class TheMamasThePapasCaliforniaDreamin {
-	name = "The Mamas & The Papas - California Dreamin";
-	sound[] = {"RomeMusic\RockClassic\TheMamas&ThePapas-CaliforniaDreamin.ogg",db+0,1};
-	duration=158;
-	musicClass = "RockClassic";
-};
-
-class TheRollingStonesSympathyForTheDevil {
-	name = "The Rolling Stones - Sympathy For The Devil";
-	sound[] = {"RomeMusic\RockClassic\TheRollingStones-SympathyForTheDevil.ogg",db+0,1};
-	duration=382;
-	musicClass = "RockClassic";
-};
-
-class When_The_War_Is_Over {
-	name = "When The War Is Over";
-	sound[] = {"RomeMusic\RockClassic\When_The_War_Is_Over.ogg",db+0,1};
-	duration=266;
-	musicClass = "RockClassic";
-};
-
-class MenAtWorkDownUnder {
-	name = "Men At Work - DownUnder";
-	sound[] = {"RomeMusic\RockClassic\MenAtWork-DownUnder.ogg",db+0,1};
-	duration=222;
-	musicClass = "RockClassic";
-};
-
-class FoolsGardenLemonTree {
-	name = "Fool's Garden - LemonTree";
-	sound[] = {"RomeMusic\RockClassic\FoolsGarden-LemonTree.ogg",db+0,1};
-	duration=193;
-	musicClass = "RockClassic";
-};
-
-class black_sabbath_war_pigs {
-	name = "Black Sabbath - War Pigs";
-	sound[] = {"RomeMusic\RockClassic\black_sabbath_war_pigs.ogg",db+0,1};
-	duration=475;
-	musicClass = "RockClassic";
-};
-
-class metallica_fuel {
-	name = "Metallica - fuel";
-	sound[] = {"RomeMusic\RockClassic\metallica_fuel.ogg",db+0,1};
-	duration=270;
-	musicClass = "RockClassic";
-};
-
-class mv_jack_stauber_buttercup {
-	name = "MV Jack Stauber Buttercup";
-	sound[] = {"RomeMusic\RockClassic\mv_jack_stauber_buttercup.ogg",db+0,1};
-	duration=208;
-	musicClass = "RockClassic";
-};
-
-class rome_one_fire {
-	name = "Rome - One Fire";
-	sound[] = {"RomeMusic\RockClassic\rome_one_fire.ogg",db+0,1};
-	duration=224;
-	musicClass = "RockClassic";
-};
-
-class seven_nation_army_lyric_video {
-	name = "Seven Nation Army - The white stripes(REMIX)";
-	sound[] = {"RomeMusic\RockClassic\seven_nation_army_lyric_video.ogg",db+0,1};
-	duration=257;
-	musicClass = "RockClassic";
-};
-
-class the_ballad_of_the_red_flame_lily {
-	name = "Rome - The ballad of the red flame lily";
-	sound[] = {"RomeMusic\RockClassic\the_ballad_of_the_red_flame_lily.ogg",db+0,1};
-	duration=278;
-	musicClass = "RockClassic";
-};
-
-class the_white_stripes_seven_nation_army {
-	name = "Seven Nation Army - The white stripes(NORMAL VERSION)";
-	sound[] = {"RomeMusic\RockClassic\the_white_stripes_-_seven_nation_army.ogg",db+0,1};
-	duration=238;
-	musicClass = "RockClassic";
-};
-
-class avenged_sevenfold_hail_to_the_king {
-	name = "Avenged Sevenfold - Hail to the king";
-	sound[] = {"RomeMusic\RockClassic\avenged_sevenfold_hail_to_the_king.ogg",db+0,1};
-	duration=314;
-	musicClass = "RockClassic";
-};
-
-class black_sabbath___paranoid__hq_ {
-	name = "Black Sabbath - Paranoid";
-	sound[] = {"RomeMusic\RockClassic\black_sabbath___paranoid__hq_.ogg",db+0,1};
-	duration=175;
-	musicClass = "RockClassic";
-};
-
-class rockthecasbah_remastered_ {
-	name = "The Clash - Rock the casbah";
-	sound[] = {"RomeMusic\RockClassic\rockthecasbah_remastered_.ogg",db+0,1};
-	duration=222;
-	musicClass = "RockClassic";
-};
-
-class rebelyell {
-	name = "Idol - Rebel yell";
-	sound[] = {"RomeMusic\RockClassic\rebelyell.ogg",db+0,1};
-	duration=289;
-	musicClass = "RockClassic";
-};
-
-class nitrousoxide {
-	name = "TerribleTim - Nitrous Oxide";
-	sound[] = {"RomeMusic\RockClassic\nitrousoxide.ogg",db+0,1};
-	duration=150;
-	musicClass = "RockClassic";
-};
-
-class dschinghiskhan_moskau {
-	name = "Dschinghis khan - Moskau";
-	sound[] = {"RomeMusic\RockClassic\dschinghiskhan_moskau.ogg",db+0,1};
-	duration=220;
-	musicClass = "RockClassic";
-};
-
-class Thunderstruck {
-	name = "AC/DC - Thunderstruck";
-	sound[] = {"RomeMusic\ACDC\Thunderstruck.ogg",db+0,1};
-	duration=292;
-	musicClass = "ACDC";
-};
-
 class BackInBlackLyrics {
 	name = "AC/DC - Back In Black";
-	sound[] = {"RomeMusic\ACDC\BackInBlackLyrics.ogg",db+0,1};
+	sound[] = {"RomeMusic\audio\rock\BackInBlackLyrics.ogg",db+0,1};
 	duration=252;
-	musicClass = "ACDC";
-};
-
-class HighwaytoHell {
-	name = "AC/DC - Highway to Hell";
-	sound[] = {"RomeMusic\ACDC\HighwaytoHell.ogg",db+0,1};
-	duration=207;
-	musicClass = "ACDC";
-};
-
-class ACDC_TNT {
-	name = "AC/DC - TNT";
-	sound[] = {"RomeMusic\ACDC\ACDC_TNT.ogg",db+0,1};
-	duration=215;
-	musicClass = "ACDC";
+	musicClass = "Rock";
 };
 
 class Hells_Bells {
 	name = "AC/DC - Hell Bells";
-	sound[] = {"RomeMusic\ACDC\Hells_Bells.ogg",db+0,1};
+	sound[] = {"RomeMusic\audio\rock\Hells_Bells.ogg",db+0,1};
 	duration=310;
-	musicClass = "ACDC";
+	musicClass = "Rock";
+};
+
+class HighwaytoHell {
+	name = "AC/DC - Highway to Hell";
+	sound[] = {"RomeMusic\audio\rock\HighwaytoHell.ogg",db+0,1};
+	duration=207;
+	musicClass = "Rock";
+};
+
+class Thunderstruck {
+	name = "AC/DC - Thunderstruck";
+	sound[] = {"RomeMusic\audio\rock\Thunderstruck.ogg",db+0,1};
+	duration=292;
+	musicClass = "Rock";
+};
+
+class ACDC_TNT {
+	name = "AC/DC - TNT";
+	sound[] = {"RomeMusic\audio\rock\ACDC_TNT.ogg",db+0,1};
+	duration=215;
+	musicClass = "Rock";
+};
+
+class FortunateSon {
+	name = "Creedence Clearwater - FortunateSon";
+	sound[] = {"RomeMusic\audio\rock\FortunateSon.ogg",db+0,1};
+	duration=141;
+	musicClass = "Rock";
+};
+
+class RockYouLikeaHurricane {
+	name = "Scorpions - Rock You Like a Hurricane";
+	sound[] = {"RomeMusic\audio\rock\RockYouLikeaHurricane.ogg",db+0,1};
+	duration=254;
+	musicClass = "Rock";
+};
+
+class Celebration {
+	name = "Kool & The Gang - Celebration";
+	sound[] = {"RomeMusic\audio\rock\Celebration.ogg",db+0,1};
+	duration=258;
+	musicClass = "Rock";
+};
+
+class BadMoonRising {
+	name = "Creedence Clearwater - Bad Moon Rising";
+	sound[] = {"RomeMusic\audio\rock\BadMoonRising.ogg",db+0,1};
+	duration=141;
+	musicClass = "Rock";
+};
+
+class RunThroughTheJungle {
+	name = "Creedence Clearwater - Run Through The Jungle";
+	sound[] = {"RomeMusic\audio\rock\RunThroughTheJungle.ogg",db+0,1};
+	duration=186;
+	musicClass = "Rock";
+};
+
+class September {
+	name = "Earth, Wind & Fire - September";
+	sound[] = {"RomeMusic\audio\rock\September.ogg",db+0,1};
+	duration=215;
+	musicClass = "Rock";
+};
+
+class TheClashShouldIStayorShouldIGo {
+	name = "The Clash - Should I Stay or Should I Go";
+	sound[] = {"RomeMusic\audio\rock\TheClash-ShouldIStayorShouldIGo.ogg",db+0,1};
+	duration=189;
+	musicClass = "Rock";
+};
+
+class StatusQuoInTheArmyNow {
+	name = "Status Quo - In The Army Now";
+	sound[] = {"RomeMusic\audio\rock\StatusQuo-InTheArmyNow.ogg",db+0,1};
+	duration=257;
+	musicClass = "Rock";
+};
+
+class StatusQuoWhateveryouwant {
+	name = "Status Quo - Whatever you want";
+	sound[] = {"RomeMusic\audio\rock\StatusQuoWhateveryouwant.ogg",db+0,1};
+	duration=261;
+	musicClass = "Rock";
+};
+
+class ColdChiselKheSanh {
+	name = "Cold Chisel - KheSanh";
+	sound[] = {"RomeMusic\audio\rock\ColdChisel-KheSanh.ogg",db+0,1};
+	duration=250;
+	musicClass = "Rock";
+};
+
+class RedgumIWasOnly19 {
+	name = "Redgum - I Was Only 19";
+	sound[] = {"RomeMusic\audio\rock\Redgum-IWasOnly19.ogg",db+0,1};
+	duration=266;
+	musicClass = "Rock";
+};
+
+class StandingOnTheOutside {
+	name = "Standing On The Outside";
+	sound[] = {"RomeMusic\audio\rock\StandingOnTheOutside.ogg",db+0,1};
+	duration=176;
+	musicClass = "Rock";
+};
+
+class thejimihendrixexperience_allalongthewatchtower {
+	name = "The Jimi Hendrix Experience - All Along The Watchtower";
+	sound[] = {"RomeMusic\audio\rock\thejimihendrixexperience_allalongthewatchtower.ogg",db+0,1};
+	duration=241;
+	musicClass = "Rock";
+};
+
+class TheMamasThePapasCaliforniaDreamin {
+	name = "The Mamas & The Papas - California Dreamin";
+	sound[] = {"RomeMusic\audio\rock\TheMamas&ThePapas-CaliforniaDreamin.ogg",db+0,1};
+	duration=158;
+	musicClass = "Rock";
+};
+
+class TheRollingStonesSympathyForTheDevil {
+	name = "The Rolling Stones - Sympathy For The Devil";
+	sound[] = {"RomeMusic\audio\rock\TheRollingStones-SympathyForTheDevil.ogg",db+0,1};
+	duration=382;
+	musicClass = "Rock";
+};
+
+class menatwork_downunder {
+	name = "Men At Work - DownUnder";
+	sound[] = {"RomeMusic\audio\rock\menatwork_downunder.ogg",db+0,1};
+	duration=222;
+	musicClass = "Rock";
+};
+
+class FoolsGardenLemonTree {
+	name = "Fool's Garden - LemonTree";
+	sound[] = {"RomeMusic\audio\rock\FoolsGarden-LemonTree.ogg",db+0,1};
+	duration=193;
+	musicClass = "Rock";
+};
+
+class black_sabbath_war_pigs {
+	name = "Black Sabbath - War Pigs";
+	sound[] = {"RomeMusic\audio\rock\black_sabbath_war_pigs.ogg",db+0,1};
+	duration=475;
+	musicClass = "Rock";
+};
+
+class metallica_fuel {
+	name = "Metallica - fuel";
+	sound[] = {"RomeMusic\audio\rock\metallica_fuel.ogg",db+0,1};
+	duration=270;
+	musicClass = "Rock";
+};
+
+class mv_jack_stauber_buttercup {
+	name = "Jack Stauber - Buttercup";
+	sound[] = {"RomeMusic\audio\rock\mv_jack_stauber_buttercup.ogg",db+0,1};
+	duration=208;
+	musicClass = "Rock";
+};
+
+class rome_onefire {
+	name = "Rome - One Fire";
+	sound[] = {"RomeMusic\audio\rock\rome_onefire.ogg",db+0,1};
+	duration=224;
+	musicClass = "Rock";
+};
+
+class seven_nation_army_lyric_video {
+	name = "Seven Nation Army - The white stripes(REMIX)";
+	sound[] = {"RomeMusic\audio\rock\seven_nation_army_lyric_video.ogg",db+0,1};
+	duration=257;
+	musicClass = "Rock";
+};
+
+class the_ballad_of_the_red_flame_lily {
+	name = "Rome - The ballad of the red flame lily";
+	sound[] = {"RomeMusic\audio\rock\the_ballad_of_the_red_flame_lily.ogg",db+0,1};
+	duration=278;
+	musicClass = "Rock";
+};
+
+class the_white_stripes_seven_nation_army {
+	name = "Seven Nation Army - The white stripes(NORMAL VERSION)";
+	sound[] = {"RomeMusic\audio\rock\the_white_stripes_-_seven_nation_army.ogg",db+0,1};
+	duration=238;
+	musicClass = "Rock";
+};
+
+class avenged_sevenfold_hail_to_the_king {
+	name = "Avenged Sevenfold - Hail to the king";
+	sound[] = {"RomeMusic\audio\rock\avenged_sevenfold_hail_to_the_king.ogg",db+0,1};
+	duration=314;
+	musicClass = "Rock";
+};
+
+class black_sabbath___paranoid__hq_ {
+	name = "Black Sabbath - Paranoid";
+	sound[] = {"RomeMusic\audio\rock\black_sabbath___paranoid__hq_.ogg",db+0,1};
+	duration=175;
+	musicClass = "Rock";
+};
+
+class rockthecasbah_remastered_ {
+	name = "The Clash - Rock the casbah";
+	sound[] = {"RomeMusic\audio\rock\rockthecasbah_remastered_.ogg",db+0,1};
+	duration=222;
+	musicClass = "Rock";
+};
+
+class rebelyell {
+	name = "Idol - Rebel yell";
+	sound[] = {"RomeMusic\audio\rock\rebelyell.ogg",db+0,1};
+	duration=289;
+	musicClass = "Rock";
+};
+
+class nitrousoxide {
+	name = "TerribleTim - Nitrous Oxide";
+	sound[] = {"RomeMusic\audio\rock\nitrousoxide.ogg",db+0,1};
+	duration=150;
+	musicClass = "Rock";
+};
+
+class dschinghiskhan_moskau {
+	name = "Dschinghis khan - Moskau";
+	sound[] = {"RomeMusic\audio\rock\dschinghiskhan_moskau.ogg",db+0,1};
+	duration=220;
+	musicClass = "Rock";
 };
