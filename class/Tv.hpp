114ab646c86a521cf6453598d0a0dc2c4e6eeaef
Alvaro Romeral <alrompin98@gmail.com>
2023-03-05 18:38:22 +0100

    Actualizado Tv

--- class/Tv.hpp
+++ class/Tv.hpp
@@ -1,147 +1,146 @@
-class The_Map_Room_Dawn
-{
-	name = "Indiana Jhones - The Map Room Dawn";
-	sound[] = {"RomeMusic\Movies\The_Map_Room_Dawn.ogg",db+0,1};
-	duration=214;
-	musicClass = "Movies";
-};
-class Band_of_Brothers
-{
-	name = "Band of Brothers - Intro theme";
-	sound[] = {"RomeMusic\Movies\Band_of_Brothers.ogg",db+0,1};
-	duration=145;
-	musicClass = "Movies";
-};
-class PacificRim
-{
-	name = "Pacific Rim - Pacific Rim";
-	sound[] = {"RomeMusic\Movies\PacificRim.ogg",db+0,1};
-	duration=294;
-	musicClass = "Movies";
-};
-class Supermarine
-{
-	name = "Dunkirk - Supermarine";
-	sound[] = {"RomeMusic\Movies\Supermarine.ogg",db+0,1};
-	duration=483;
-	musicClass = "Movies";
-};
-class Godzilla
-{
-	name = "Godzilla - Godzilla Theme";
-	sound[] = {"RomeMusic\Movies\Godzilla.ogg",db+0,1};
-	duration=126;
-	musicClass = "Movies";
-};
-class NowWeAreFree
-{
-	name = "Gladiator - Now We Are Free";
-	sound[] = {"RomeMusic\Movies\NowWeAreFree.ogg",db+0,1};
-	duration=254;
-	musicClass = "Movies";
-};
-class TheGoodtheBadandtheUgly
-{
-	name = "The Good the Bad and the Ugly";
-	sound[] = {"RomeMusic\Movies\TheGoodtheBadandtheUgly.ogg",db+0,1};
-	duration=173;
-	musicClass = "Movies";
-};
-class ForAFewDollars
-{
-	name = "For A Few Dollars";
-	sound[] = {"RomeMusic\Movies\ForAFewDollars.ogg",db+0,1};
-	duration=170;
-	musicClass = "Movies";
-};
-class 28days
-{
+class 28days_theme {
 	name = "28 Days Later - In the House, In the Heartbeat";
-	sound[] = {"RomeMusic\Movies\28days.ogg",db+0,1};
+	sound[] = {"RomeMusic\audio\tv\28days_theme.ogg",db+0,1};
 	duration=258;
-	musicClass = "Movies";
+	musicClass = "Tv";
 };
-class ReunionInTheTent
-{
-	name = "Indiana Jones - Reunion In The Tent";
-	sound[] = {"RomeMusic\Movies\ReunionInTheTent.ogg",db+0,1};
-	duration=241;
-	musicClass = "Movies";
-};
-class clubbedtodeath
-{
-	name = "Matrix - Clubbed to death";
-	sound[] = {"RomeMusic\Movies\clubbedtodeath.ogg",db+0,1};
-	duration=455;
-	musicClass = "Movies";
-};
-class April1945
-{
-	name = "Fury - April 1945";
-	sound[] = {"RomeMusic\Movies\April1945.ogg",db+0,1};
-	duration=255;
-	musicClass = "Movies";
-};
-class TheMandalorian
-{
-	name = "The Mandalorian";
-	sound[] = {"RomeMusic\Movies\TheMandalorian.ogg",db+0,1};
-	duration=198;
-	musicClass = "Movies";
-};
-class TheMandalorian_OrchestralVersion
-{
-	name = "The Mandalorian (OrchestralVersion)";
-	sound[] = {"RomeMusic\Movies\TheMandalorian_OrchestralVersion.ogg",db+0,1};
-	duration=140;
-	musicClass = "Movies";
-};
-class GeraltOfRiviaTheWitcherOST
-{
-	name = "The Witcher - Geralt Of Rivia";
-	sound[] = {"RomeMusic\Movies\GeraltOfRiviaTheWitcherOST.ogg",db+0,1};
-	duration=146;
-	musicClass = "Movies";
-};
-class TossACoinToYourWitcher
-{
-	name = "The Witcher - Toss A Coin To Your Witcher";
-	sound[] = {"RomeMusic\Movies\TossACoinToYourWitcher.ogg",db+0,1};
-	duration=195;
-	musicClass = "Movies";
-};
-class StarshipTroopersItsAGoodDayToDie
-{
-	name = "Starship Troopers - Its A Good Day To Die";
-	sound[] = {"RomeMusic\Movies\StarshipTroopersItsAGoodDayToDie.ogg",db+0,1};
-	duration=268;
-	musicClass = "Movies";
-};
-class StarshipTroopersTheme
-{
-	name = "Starship Troopers - Theme";
-	sound[] = {"RomeMusic\Movies\StarshipTroopersTheme.ogg",db+0,1};
-	duration=150;
-	musicClass = "Movies";
-};
-class ABridgeTooFarTheme
-{
+
+class abridgetoofar_theme {
 	name = "A Bridge Too Far - Theme";
-	sound[] = {"RomeMusic\Movies\A_Bridge_Too_Far-Theme.ogg",db+0,1};
+	sound[] = {"RomeMusic\audio\tv\abridgetoofar_theme.ogg",db+0,1};
 	duration=234;
-	musicClass = "Movies";
+	musicClass = "Tv";
 };
-class TopGunAnthem
-{
-	name = "Top Gun - Anthem";
-	sound[] = {"RomeMusic\Movies\TopGunAnthem.ogg",db+0,1};
+
+class bandofbrothers_theme {
+	name = "Band of Brothers - Theme";
+	sound[] = {"RomeMusic\audio\tv\bandofbrothers_theme.ogg",db+0,1};
+	duration=145;
+	musicClass = "Tv";
+};
+
+class dunkirk_supermarine {
+	name = "Dunkirk - Supermarine";
+	sound[] = {"RomeMusic\audio\tv\dunkirk_supermarine.ogg",db+0,1};
+	duration=483;
+	musicClass = "Tv";
+};
+
+class forafewdollars_theme {
+	name = "For A Few Dollars - Theme";
+	sound[] = {"RomeMusic\audio\tv\forafewdollars_theme.ogg",db+0,1};
+	duration=170;
+	musicClass = "Tv";
+};
+
+class fury_april1945 {
+	name = "Fury - April 1945";
+	sound[] = {"RomeMusic\audio\tv\fury_april1945.ogg",db+0,1};
+	duration=255;
+	musicClass = "Tv";
+};
+
+class gladiator_nowwearefree {
+	name = "Gladiator - Now We Are Free";
+	sound[] = {"RomeMusic\audio\tv\gladiator_nowwearefree.ogg",db+0,1};
 	duration=254;
-	musicClass = "Movies";
+	musicClass = "Tv";
 };
-class TopGunDangerZone
-{
+
+class godzilla_theme {
+	name = "Godzilla - Godzilla Theme";
+	sound[] = {"RomeMusic\audio\tv\godzilla_theme.ogg",db+0,1};
+	duration=126;
+	musicClass = "Tv";
+};
+
+class indianajhones_reunioninthetent {
+	name = "Indiana Jones - Reunion In The Tent";
+	sound[] = {"RomeMusic\audio\tv\indianajhones_reunioninthetent.ogg",db+0,1};
+	duration=241;
+	musicClass = "Tv";
+};
+
+class indianajhones_themaproomdawn {
+	name = "Indiana Jhones - The Map Room Dawn";
+	sound[] = {"RomeMusic\audio\tv\indianajhones_themaproomdawn.ogg",db+0,1};
+	duration=214;
+	musicClass = "Tv";
+};
+
+class matrix_clubbedtodeath {
+	name = "Matrix - Clubbed to death";
+	sound[] = {"RomeMusic\audio\tv\matrix_clubbedtodeath.ogg",db+0,1};
+	duration=455;
+	musicClass = "Tv";
+};
+
+class pacificrim_theme {
+	name = "Pacific Rim - Pacific Rim";
+	sound[] = {"RomeMusic\audio\tv\pacificrim_theme.ogg",db+0,1};
+	duration=294;
+	musicClass = "Tv";
+};
+
+class starshiptroopers_itsagooddaytodie {
+	name = "Starship Troopers - Its A Good Day To Die";
+	sound[] = {"RomeMusic\audio\tv\starshiptroopers_itsagooddaytodie.ogg",db+0,1};
+	duration=268;
+	musicClass = "Tv";
+};
+
+class starshiptroopers_theme {
+	name = "Starship Troopers - Theme";
+	sound[] = {"RomeMusic\audio\tv\starshiptroopers_theme.ogg",db+0,1};
+	duration=150;
+	musicClass = "Tv";
+};
+
+class thegoodthebadandtheugly_theme {
+	name = "The Good the Bad and the Ugly";
+	sound[] = {"RomeMusic\audio\tv\thegoodthebadandtheugly_theme.ogg",db+0,1};
+	duration=173;
+	musicClass = "Tv";
+};
+
+class themandalorian_theme {
+	name = "The Mandalorian";
+	sound[] = {"RomeMusic\audio\tv\themandalorian_theme.ogg",db+0,1};
+	duration=198;
+	musicClass = "Tv";
+};
+
+class themandalorian_themeorchestral {
+	name = "The Mandalorian (OrchestralVersion)";
+	sound[] = {"RomeMusic\audio\tv\themandalorian_themeorchestral.ogg",db+0,1};
+	duration=140;
+	musicClass = "Tv";
+};
+
+class thewitcher_theme {
+	name = "The Witcher - Geralt Of Rivia";
+	sound[] = {"RomeMusic\audio\tv\thewitcher_theme.ogg",db+0,1};
+	duration=146;
+	musicClass = "Tv";
+};
+
+class thewitcher_tossacointoyourwitcher {
+	name = "The Witcher - Toss A Coin To Your Witcher";
+	sound[] = {"RomeMusic\audio\tv\thewitcher_tossacointoyourwitcher.ogg",db+0,1};
+	duration=195;
+	musicClass = "Tv";
+};
+
+class topgun_anthem {
+	name = "Top Gun - Anthem";
+	sound[] = {"RomeMusic\audio\tv\topgun_anthem.ogg",db+0,1};
+	duration=254;
+	musicClass = "Tv";
+};
+
+class topgun_dangerzone {
 	name = "Top Gun - Danger Zone";
-	sound[] = {"RomeMusic\Movies\TopGun-DangerZone.ogg",db+0,1};
+	sound[] = {"RomeMusic\audio\tv\topgun_dangerzone.ogg",db+0,1};
 	duration=213;
-	musicClass = "Movies";
+	musicClass = "Tv";
 };
